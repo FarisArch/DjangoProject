df494d3233fd40bb829e867bae3cbb8c29c432de
faris <farisaimanCS@protonmail.com>
2021-03-18 16:29:10 +0800

    Added CSS

--- Blog/urls.py
+++ Blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 # Add url patterns below
 urlpatterns = [
     path('',views.post_list,name='post-list'),
+    path('post/<int:pk>/',views.post_detail,name='post_detail'),
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Post
+
 # Create your views here.
+
+
 def post_list(request):
     posts = Post.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')
     return render(request, 'Blog/post_list.html',{'posts':posts})
+
+
+def post_detail(request,pk):
+    post= get_object_or_404(Post,pk=pk)
+    return render(request,'Blog/post_details.html',{'post':post})
